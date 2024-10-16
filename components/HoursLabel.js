a0cd363f1554274e3c25d0c1f19e1c4ff05a5771
antorower <antonis.mastorakis@gmail.com>
2024-10-16 07:03:38 +0300

    wres

--- components/HoursLabel.js
+++ components/HoursLabel.js
@@ -3,8 +3,8 @@ const HoursLabel = () => {
     <div className="relative">
       <div className="w-[60%] h-[60%] bg-primary absolute top-7 left-7 rounded-full -z-20 animate-ping"></div>
       <div className="rounded-full bg-black border-[2px] border-primary w-[140px] h-[140px] flex flex-col gap-1 justify-center items-center">
-        <div className="text-center text-xl font-bold text-gray-300">OPENING</div>
-        <div className="text-center text-xl font-bold text-gray-300">HOURS</div>
+        <div className="text-center text-xl font-bold text-primary">OPENING</div>
+        <div className="text-center text-xl font-bold text-primary">HOURS</div>
       </div>
     </div>
   );
